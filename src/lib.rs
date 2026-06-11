e126736e6c2daea2ffe79707b0380645515eea2e
Cirious Studio <cirious.studio@gmail.com>
2026-06-11 04:39:02 -0300

    fix: update docs.rs configuration to fix build failure

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@
 //! ```
 
 // Enables docs.rs features to show tags like "Only on Windows"
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 // Ensures all public items are documented (Essential for crates.io)
 #![warn(missing_docs)]
 // Prevents accidental unsafe code in the entire crate
